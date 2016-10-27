68e6836e96c10cf728e39138dc5c92e127e35d27
'anthonyhombiat' <'anthony.hombiat@imag.fr'>
2016-10-27 11:51:14 +0200

    - forgot to add these 2 files to the git index :3

---
+++ src/main/java/lig/steamer/of4osm/io/OF4OSMOntoReaderLifecycleStates.java
@@ -0,0 +1,35 @@
+package lig.steamer.of4osm.io;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.commons.lang3.time.DurationFormatUtils;
+
+import lig.steamer.of4osm.IOF4OSMOntology;
+import lig.steamer.of4osm.core.folkso.tag.key.impl.LifecycleState;
+import lig.steamer.of4osm.core.onto.meta.IHighLevelConcept;
+import lig.steamer.of4osm.core.onto.meta.IHighLevelConceptParent;
+import lig.steamer.of4osm.util.OF4OSMConceptFactory;
+
+public final class OF4OSMOntoReaderLifecycleStates {
+
+	private static final Logger LOGGER = Logger.getLogger(OF4OSMOntoReaderLifecycleStates.class.getName());
+	
+	public static void read(LifecycleState[] states, IOF4OSMOntology of4osm){
+		
+		final long t0 = System.currentTimeMillis();
+		LOGGER.log(Level.INFO, "Adding lifecycle states to of4osm...");
+		
+		IHighLevelConcept statefulConcept = OF4OSMConceptFactory.createHighLevelConcept("StatefulEntity");
+		
+		for(LifecycleState state : states){
+			IHighLevelConcept concept = OF4OSMConceptFactory.createHighLevelConcept(state.name() + "Entity");
+			concept.addParent((IHighLevelConceptParent) statefulConcept);
+			of4osm.addConcept(concept);
+		}
+		
+		final long t1 = System.currentTimeMillis();
+		LOGGER.log(Level.INFO, "Adding lifecycle states done (" + DurationFormatUtils.formatDurationHMS(t1 - t0) + ").");
+	}
+	
+}
---
+++ src/main/java/lig/steamer/of4osm/util/OF4OSMVocabulary.java
@@ -0,0 +1,21 @@
+package lig.steamer.of4osm.util;
+
+public final class OF4OSMVocabulary {
+
+	public static final String NAMESPACE = "http://steamer.imag.fr/of4osm";
+	
+	public static final String HIGHLEVELCONCEPT_PATH = "/highlevelconcept";
+	public static final String OSMTAGKEYCONCEPT_PATH = "/osmtagkeyconcept";
+	public static final String OSMTAGCONCEPT_PATH = "/osmtagconcept";
+	public static final String OSMTAGCOMBINATION_PATH = "/osmtagcombination";
+	
+	public static final String HIGHLEVELCONCEPT_IRI = NAMESPACE + HIGHLEVELCONCEPT_PATH;
+	public static final String OSMTAGKEYCONCEPT_IRI = NAMESPACE + OSMTAGKEYCONCEPT_PATH;
+	public static final String OSMTAGCONCEPT_IRI = NAMESPACE + OSMTAGCONCEPT_PATH;
+	public static final String OSMTAGCOMBINATION_IRI = NAMESPACE + OSMTAGCOMBINATION_PATH;
+	
+	public static final String IRI_SEPARATOR = "#";
+	
+	public static final String DEFAULT_LANG = "EN";
+	
+}
